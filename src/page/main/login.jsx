18b60796be9d187ee326b009486854464e7f41b4
150018405@qq.com <150018405@qq.com>
2018-08-14 23:12:28 +0800

    up

--- src/page/main/login.jsx
+++ src/page/main/login.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+
+import { Button, Row, Col, message } from 'antd'
+
 import Logo from '@/common/commponent/logo'
 export default class Login extends Component {
   constructor (props) {
@@ -11,15 +14,35 @@ export default class Login extends Component {
 
   }
 
+  login () {
+    message.error('请输入您的登录账号！');
+  }
+
   render () {
     return (
       <div className="page login">
         <div className="login_main">
           <Logo></Logo>
-          <div className="form_list">
-            <div className="form_item">
-              <label>帐号：</label>
-              <input placeholder="请输入您的登录账号"/>
+          <div className="login_box">
+            <div className="form_list">
+              <div className="form_item">
+                <label>帐号：</label>
+                <input placeholder="请输入您的登录账号"/>
+              </div>
+              <div className="form_item">
+                <label>密码：</label>
+                <input placeholder="请输入您的登录密码"/>
+              </div>
+              <div className="form_item">
+                <Row type="flex" justify="space-around">
+                  <Col  span={8}>
+                    <Button type="primary" block={true} onClick={() => this.login()}>登   录</Button>
+                  </Col>
+                  <Col  span={8} offset={2}>
+                    <Button block={true}>取消</Button>
+                  </Col>
+                </Row>
+              </div>
             </div>
           </div>
         </div>
